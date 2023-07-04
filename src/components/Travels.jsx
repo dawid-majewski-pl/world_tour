84b8d8729c54ea4c6c0b6828f58fcc860c09cc37
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-07-04 11:33:55 +0200

    Sorting by region and price added.

--- src/components/Travels.jsx
+++ src/components/Travels.jsx
@@ -6,34 +6,52 @@ const Travels = () => {
 
     const [travels, setTravels] = useState(data)
 
+    // Filter type 
+    const filterType = (category) => {
+        setTravels(
+            data.filter((item) => {
+                return item.category === category;
+            })
+        );
+    };
+
+        // Filter type 
+        const filterPrice = (price) => {
+            setTravels(
+                data.filter((item) => {
+                    return item.price === price;
+                })
+            );
+        };
+
     return (
         <div className='max-w-[1640px] m-auto px-4 py-12'>
            <h2 className='text-orange-400 font-bold text-4xl text-center'>Travels</h2>
            
            {/* Filter section */}
-            <div className='flex flex-col lg:flex-row justify-between mx-6'>
+            <div className='flex flex-col lg:flex-row justify-between mx-2'>
                 {/* Filter Type */}
                 <div className='mx-auto lg:m-0'>
-                    <p className='font-bold text-gray-500 py-2 text-center lg:text-left'>Filter type</p>
+                    <p className='font-bold text-gray-500 py-2 text-center lg:text-left lg:pl-2'>Filter type</p>
                     <div className='flex justify-between max-w-[640px] flex-wrap gap-2'>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>All</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Europe</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Asia</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Africa</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>South America</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>North America</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Australia</button>
+                        <button onClick={()=> setTravels(data)} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>All</button>
+                        <button onClick={()=> filterType('Europe')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Europe</button>
+                        <button onClick={()=> filterType('Asia')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Asia</button>
+                        <button onClick={()=> filterType('Africa')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Africa</button>
+                        <button onClick={()=> filterType('South America')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>South America</button>
+                        <button onClick={()=> filterType('North America')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>North America</button>
+                        <button onClick={()=> filterType('Australia')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>Australia</button>
                     </div>
                 </div>
 
                 {/* Filter Price */}
                 <div className='mx-auto lg:m-0'>
-                    <p className='font-bold text-gray-500 py-2 text-center lg:text-right'>Filter Price</p>
+                    <p className='font-bold text-gray-500 py-2 text-center lg:text-right lg:pr-2'>Filter Price</p>
                     <div className='flex justify-between max-w-[240px] gap-2'>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>£</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>££</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>£££</button>
-                        <button className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>££££</button>
+                        <button onClick={()=> filterPrice('£')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>£</button>
+                        <button onClick={()=> filterPrice('££')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>££</button>
+                        <button onClick={()=> filterPrice('£££')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>£££</button>
+                        <button onClick={()=> filterPrice('££££')} className='border-orange-400 bg-blue-400 rounded-full duration-300 px-3 py-1 hover:text-white hover:bg-blue-500'>££££</button>
                     </div>
                 </div>
             </div>
